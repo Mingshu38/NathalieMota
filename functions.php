c6c9d16ddc4f91277f07549ac01946c1e4f025cb
Joly Sonia <sonia.joly38@gmail.com>
2024-03-20 16:35:12 +0100

    Creation add_menu and logo header

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+// Style personnalisé du thème 
+function nathalie_mota_theme_enqueue(){
+    wp_enqueue_syle('nathalie-mota', get_stylesheet_uri());
+    wp_enqueue_style('theme-style', get_template_directory_uri() . '/assets/css/style.css');
+    filemtime(get_stylesheet_directory() . '/css/theme.css');
+}
+add_action('wp_enqueue_scripts','nathalie-mota-theme_enqueue');
+// Gestion du menu 
+
+// créer un lien pour la gestion des menus dans l'administration et activation d'un menu pour le header et d'un menu pour le footer.
+function register_my_menu(){
+    register_nav_menu('main', "Menu principal");
+    register_nav_menu('footer', "Menu pied de page");
+ }
+ add_action('after_setup_theme', 'register_my_menu');
+
+?>
--- header.php
+++ header.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang=<?php language_attributes(); ?>> <!-- Définit automatiquement la langue -->
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>"> <!-- Permet de définir l'encodage du site -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php wp_head()?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+    <?php wp_head(); ?>
 </head>
-<body>
-    
+<body <?php body_class(); ?>> <!-- Permet d’obtenir des noms de classe CSS en fonction de la page visitée -->
+
+    <?php wp_body_open(); ?>
+        <header id="header">
+            <div class="logo">
+                <a href="http://nathaliemota.local"><img class="logo-img" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Logo"></a>
+                <?php 
+				// Affichage du menu main déclaré dans functions.php
+					wp_nav_menu(array('theme_location' => 'main')); 
+				?>             
+            </div>
+            
+        </header>
